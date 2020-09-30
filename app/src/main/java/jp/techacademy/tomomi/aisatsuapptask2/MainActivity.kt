f4e1cb1e923869182cab1d3f52a28e4ce5fa7d35
tomomi.koyama <smalllamp.tk@gmail.com>
2020-09-30 22:50:31 +0900

    ButtonをタッチするとTimePickerDialogを表示して，設定した時刻に応じてTextViewに表示されるあいさつを設定。

---
+++ app/src/main/java/jp/techacademy/tomomi/aisatsuapptask2/MainActivity.kt
@@ -0,0 +1,38 @@
+package jp.techacademy.tomomi.aisatsuapptask2
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener() {
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { Activity, hour, minute ->
+                if (hour >= 2 && hour <= 9) {
+                    textView.text = "おはよう"
+                } else if (hour >= 10 && hour <= 17) {
+                    textView.text = "こんにちは"
+                } else {
+                    textView.text = "こんばんは"
+                }
+            },
+            13, 0, true
+        )
+
+        timePickerDialog.show()
+
+    }
+
+}
